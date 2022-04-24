541fd1b5b744a7f6a51be47c82b1a64b9c7df32c
zhuangpei <zhuang.p@northeastern.edu>
2022-04-24 00:49:20 -0700

    2-6 开发Hello World 接口 访问地址：http://127.0.0.1:8880/hello

--- src/main/java/com/pei/wiki/config/WikiApplication.java
+++ src/main/java/com/pei/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.pei.wiki;
+package com.pei.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.pei")
 @SpringBootApplication
 public class WikiApplication {
 
---
+++ src/main/java/com/pei/wiki/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.pei.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    // http://127.0.0.1:8880/hello
+    @GetMapping ("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
