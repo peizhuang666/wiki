886b78b14010b564102996afa065518a63d32b9c
zhuangpei <zhuang.p@northeastern.edu>
2022-04-24 23:45:35 -0700

    2-7 增加hello 接口测试test.http

--- src/main/java/com/pei/wiki/controller/TestController.java
+++ src/main/java/com/pei/wiki/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.pei.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -11,6 +14,11 @@ public class TestController {
     @GetMapping ("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
     }
 
 }
